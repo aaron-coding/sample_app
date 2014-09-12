18d69fdba892d9fe2a67e5c9e42a20eec0171639
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-09-11 17:50:38 -0700

    some changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  let(:base_title) {"Ruby on Rails"}
+  
   describe "Home page" do
     
     it "should have the content 'Sample App' in title and page" do
@@ -16,7 +18,7 @@ describe "StaticPages" do
     it "should include the content 'Help' in title and page" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
-      expect(page).to have_title("Help")
+      expect(page).to have_title(" #{base_title} | Help")
     end 
   end 
   
@@ -24,11 +26,17 @@ describe "StaticPages" do
     it "should include minimally 'About Us' in title and page" do
       visit '/static_pages/about'
       expect(page).to have_content("About Us")
-      expect(page).to have_title("About Us")
+      expect(page).to have_title(" #{base_title} | About Us")
     end
   end 
   
-
+  describe "Contact us page" do 
+    it "should include Contact in title and on the page" do
+      visit '/static_pages/contact'
+      expect(page).to have_content("Contact")
+      expect(page).to have_title("Contact")
+    end 
+  end 
   
   
 end
