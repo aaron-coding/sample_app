97b5e1dea36f9f7c1d5503499c5f807d26762f1d
Aaron Cordovez <aaron.cordovez.m@gmail.com>
2014-09-29 16:23:19 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Aaron Cordovez"
+    email     "aaron.cordovez.m@example.com"
+    password  "foobar"
+    password_confirmation "foobar"
+  end 
+end 
+    
